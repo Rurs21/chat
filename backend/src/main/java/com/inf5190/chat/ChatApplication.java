5f4024762220e2bffd3f5b257e967f3fd65f4a8e
Rurs21 <rurs21@protonmail.com>
2022-10-24 13:26:54 -0400

    Add & Use FireStore Login

--- backend/src/main/java/com/inf5190/chat/ChatApplication.java
+++ backend/src/main/java/com/inf5190/chat/ChatApplication.java
@@ -1,5 +1,12 @@
 package com.inf5190.chat;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -10,14 +17,32 @@ import com.inf5190.chat.auth.filter.AuthFilter;
 import com.inf5190.chat.auth.session.SessionDataAccessor;
 import com.inf5190.chat.auth.session.SessionManager;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  * Application spring boot.
  */
 @SpringBootApplication
 public class ChatApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ChatApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
+		try {
+			if (FirebaseApp.getApps().size() == 0) {
+				FileInputStream serviceAccount = new
+						FileInputStream("firebase-key.json");
+				FirebaseOptions options = FirebaseOptions.builder()
+						.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+						.build();
+				LOGGER.info("Initializing Firebase application.");
+				FirebaseApp.initializeApp(options);
+			}
+			LOGGER.info("Firebase application already initialized.");
+			SpringApplication.run(ChatApplication.class, args);
+		} catch (IOException e) {
+			System.err.println("Could not initialise application. Please check you service account key path");
+		}
 	}
 
 	/**
--- backend/src/main/java/com/inf5190/chat/auth/AuthController.java
+++ backend/src/main/java/com/inf5190/chat/auth/AuthController.java
@@ -2,6 +2,8 @@ package com.inf5190.chat.auth;
 
 import javax.servlet.ServletContext;
 
+import com.inf5190.chat.auth.repository.FirestoreUserAccount;
+import com.inf5190.chat.auth.repository.UserAccountRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,8 @@ import com.inf5190.chat.auth.session.SessionData;
 import com.inf5190.chat.auth.session.SessionDataAccessor;
 import com.inf5190.chat.auth.session.SessionManager;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * Contrôleur qui gère l'API de login et logout.
  * 
@@ -21,17 +25,25 @@ import com.inf5190.chat.auth.session.SessionManager;
 @RestController()
 public class AuthController implements ServletContextAware {
 
+    private UserAccountRepository userAccountRepository;
     private final SessionManager sessionManager;
     private final SessionDataAccessor sessionDataAccessor;
     private ServletContext servletContext;
 
-    public AuthController(SessionManager sessionManager, SessionDataAccessor sessionDataAccessor) {
+    public AuthController(UserAccountRepository userAccountRepository, SessionManager sessionManager, SessionDataAccessor sessionDataAccessor) {
+        this.userAccountRepository = userAccountRepository;
         this.sessionManager = sessionManager;
         this.sessionDataAccessor = sessionDataAccessor;
     }
 
     @PostMapping("auth/login")
-    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) throws ExecutionException, InterruptedException {
+        FirestoreUserAccount user = userAccountRepository.getUserAccount(loginRequest.username());
+        if (user == null) {
+            FirestoreUserAccount newUser = new FirestoreUserAccount(loginRequest.username(), loginRequest.password());
+            userAccountRepository.setUserAccount(newUser);
+        }
+
         SessionData userSession = new SessionData(loginRequest.username());
         String token = this.sessionManager.addSession(userSession);
         return new LoginResponse(token);
