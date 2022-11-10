88c62fa3c62b2569c3eb1e2f0f338423365be8fd
Rurs21 <rurs21@protonmail.com>
2022-11-09 19:02:06 -0500

    json web token

--- backend/src/main/java/com/inf5190/chat/auth/session/SessionManager.java
+++ backend/src/main/java/com/inf5190/chat/auth/session/SessionManager.java
@@ -1,10 +1,19 @@
 package com.inf5190.chat.auth.session;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.stereotype.Repository;
+
+import javax.crypto.SecretKey;
 
 /**
  * Classe qui gère les sessions utilisateur.
@@ -15,9 +24,17 @@ import org.springframework.stereotype.Repository;
 public class SessionManager {
 
     private final Map<String, SessionData> sessions = new HashMap<String, SessionData>();
+    private static final String SECRET_KEY_BASE64 = "ZUUuPDo1zw1K9dCBuU+VetaGi3VhfmJE9y15NTaWWjs=";
+    private final SecretKey secretKey;
+    private final JwtParser jwtParser;
+
+    public SessionManager() {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY_BASE64));
+        this.jwtParser = Jwts.parserBuilder().setSigningKey(this.secretKey).build();
+    }
 
     public String addSession(SessionData authData) {
-        final String token = this.generateToken();
+        final String token = this.generateToken(authData.username());
         this.sessions.put(token, authData);
         return token;
     }
@@ -27,11 +44,25 @@ public class SessionManager {
     }
 
     public SessionData getSession(String token) {
-        return this.sessions.get(token);
+        try {
+            Jws<Claims> jws = jwtParser.parseClaimsJws(token);
+            return this.sessions.get(token);
+        } catch (JwtException ex) {
+            return null;
+        }
     }
 
-    private String generateToken() {
-        return UUID.randomUUID().toString();
+    private String generateToken(String username) {
+        Instant now = Instant.now();
+        Instant expiration = now.plusSeconds(7200);
+
+        return Jwts.builder()
+                //.setAudience("localhost")
+                .setIssuedAt(new Date(now.toEpochMilli()))
+                .setSubject(username)
+                .setExpiration(new Date(expiration.toEpochMilli()))
+                .signWith(this.secretKey)
+                .compact();
     }
 
 }
