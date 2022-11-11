7be1a57e7259ec7103d47407e8a805edefcd7a0a
Rurs21 <rurs21@protonmail.com>
2022-11-11 17:57:19 -0500

    Use Firestore for chat messages

--- backend/src/main/java/com/inf5190/chat/messages/MessageController.java
+++ backend/src/main/java/com/inf5190/chat/messages/MessageController.java
@@ -1,7 +1,9 @@
 package com.inf5190.chat.messages;
 
+import com.google.cloud.Timestamp;
 import com.inf5190.chat.auth.session.SessionDataAccessor;
 import com.inf5190.chat.messages.model.Message;
+import com.inf5190.chat.messages.repository.FirestoreMessage;
 import com.inf5190.chat.messages.repository.MessageRepository;
 import com.inf5190.chat.websocket.WebSocketManager;
 
@@ -12,6 +14,7 @@ import org.springframework.web.context.ServletContextAware;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Contrôleur qui gère l'API de messages.
@@ -36,13 +39,14 @@ public class MessageController implements ServletContextAware {
     }
 
     @GetMapping(ROOT_PATH)
-    public List<Message> getMessages(@RequestParam Optional<Long> fromId) {
+    public List<Message> getMessages(@RequestParam Optional<String> fromId) throws ExecutionException, InterruptedException {
         return messageRepository.getMessages(fromId);
     }
 
     @PostMapping(ROOT_PATH)
-    public void createMessage(@RequestBody Message createMessageRequest) {
-        messageRepository.createMessage(createMessageRequest);
+    public void createMessage(@RequestBody Message createMessageRequest) throws ExecutionException, InterruptedException {
+        FirestoreMessage newMessage = new FirestoreMessage(createMessageRequest.username(), Timestamp.now(), createMessageRequest.text());
+        messageRepository.createMessage(newMessage);
         webSocketManager.notifySessions();
     }
 
--- backend/src/main/java/com/inf5190/chat/messages/repository/MessageRepository.java
+++ backend/src/main/java/com/inf5190/chat/messages/repository/MessageRepository.java
@@ -1,38 +1,71 @@
 package com.inf5190.chat.messages.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-
-import com.inf5190.chat.messages.model.Message;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Repository;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
+
+import com.inf5190.chat.messages.model.Message;
 /**
  * Classe qui gère la persistence des messages.
- * 
+ *
  * En mémoire pour le moment.
  */
 @Repository
 public class MessageRepository {
-    private final List<Message> messages = new ArrayList<Message>();
-    private final AtomicLong idGenerator = new AtomicLong(0);
 
-    public List<Message> getMessages(Optional<Long> fromId) {
+    private static final String COLLECTION_NAME = "chatMessages";
+    private static final int MESSAGES_LIMIT = 20;
+
+    private final Firestore firestore = FirestoreClient.getFirestore();
+
+
+
+    public List<Message> getMessages(Optional<String> fromId) throws ExecutionException, InterruptedException {
+        final CollectionReference collectionRef = firestore.collection(COLLECTION_NAME);
+
+        ApiFuture<QuerySnapshot> getFuture;
+
         if (fromId.isPresent()) {
-            int fromIdx = fromId.get().intValue() + 1;
-            return this.messages.subList(fromIdx , this.messages.size());
+            DocumentSnapshot messageFromId = collectionRef.document(fromId.get()).get().get();
+            getFuture = collectionRef.orderBy("timestamp").startAfter(messageFromId).get();
         } else {
-            return this.messages;
+            getFuture = collectionRef.orderBy("timestamp").limit(MESSAGES_LIMIT).get();
+        }
+
+        final QuerySnapshot messagesSnapshot = getFuture.get();
+        if (messagesSnapshot.size() > 0) {
+            return messagesSnapshot.getDocuments().stream()
+                    .map(document -> {
+                        FirestoreMessage storeMessage = document.toObject(FirestoreMessage.class);
+                        return new Message(document.getId(),
+                                storeMessage.getUsername(),
+                                storeMessage.getTimestamp().getSeconds(),
+                                storeMessage.getText());
+                    }).collect(Collectors.toList());
+        } else {
+            return null;
         }
     }
 
-    public Message createMessage(Message message) {
-        long id = idGenerator.getAndIncrement();
-        Message newMessage = message.withId(id);
-        this.messages.add((int)id, newMessage);
+    public Message createMessage(FirestoreMessage message) throws InterruptedException, ExecutionException {
+        final CollectionReference collectionRef = firestore.collection(COLLECTION_NAME);
+
+        final DocumentReference docRef = collectionRef.document();
+
+        final ApiFuture<WriteResult> apiFuture = docRef.create(message);
+
+        final WriteResult result = apiFuture.get();
+        System.out.println("Operation timestamp: " + result.getUpdateTime().toDate().getTime());
+
+        final Message newMessage = new Message(docRef.getId(), message.getUsername(), message.getTimestamp().getSeconds(), message.getText());
+
         return newMessage;
     }
-
 }
