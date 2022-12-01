2381f64bae26ac52466ea972ecead07b7f190dd5
Rurs21 <rurs21@protonmail.com>
2022-12-01 12:17:01 -0500

    Add login error message

--- frontend/src/app/login/login-page/login-page.component.ts
+++ frontend/src/app/login/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -8,12 +9,21 @@ import { LoginService } from '../login.service';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  loginError : null | String = null;
   constructor(private router: Router, private loginService: LoginService) {}
 
   ngOnInit(): void {}
 
   async onLogin(login: { username: string; password: string }) {
-    await this.loginService.login(login);
-    this.router.navigate(['/chat']);
+    try {
+      await this.loginService.login(login);
+      this.router.navigate(['/chat']);
+    } catch (e) {
+      if (e instanceof HttpErrorResponse && e.status == 403) {
+        this.loginError = "Mot de passe invalide";
+      } else {
+        this.loginError = "Problème de connexion";
+      }
+    }
   }
 }
