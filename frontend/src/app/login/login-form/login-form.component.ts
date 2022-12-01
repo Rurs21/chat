2529cc91435dad3e34005be4f51b32811baf94d2
Rurs21 <rurs21@protonmail.com>
2022-11-30 19:38:32 -0500

    Add form validation loginfForm

--- frontend/src/app/login/login-form/login-form.component.ts
+++ frontend/src/app/login/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
@@ -7,15 +7,27 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
-  loginForm = this.fb.group({
-    username: '',
-    password: '',
+  loginForm : FormGroup = this.fb.group({
+    username: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
   });
 
   @Output()
   login = new EventEmitter<{ username: string; password: string }>();
 
   constructor(private fb: FormBuilder) {}
+
+  get username() { return this.loginForm.get('username'); }
+
+  get password() { return this.loginForm.get('password'); }
+
+  showUsernameRequiredError(): boolean {
+    return this.showError('username', 'required');
+  }
+
+  showPasswordRequiredError(): boolean {
+    return this.showError('password', 'required');
+  }
 
   ngOnInit(): void {}
 
@@ -29,6 +41,16 @@ export class LoginFormComponent implements OnInit {
         username: this.loginForm.value.username,
         password: this.loginForm.value.password,
       });
+    } else {
+      this.loginForm.markAllAsTouched();
     }
   }
+
+  private showError(field: 'username' | 'password', error: string): boolean {
+    return (
+      this.loginForm.controls[field].hasError(error) &&
+      (this.loginForm.controls[field].dirty || this.loginForm.controls[field].touched)
+    );
+  }
+
 }
