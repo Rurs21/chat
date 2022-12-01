2dc82766686f32e6dfbe7d455bcb5c7a9d88341d
Rurs21 <rurs21@protonmail.com>
2022-12-01 12:32:16 -0500

    Add chat-page guard
    redirect to login if user is not logged in

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatPageComponent } from './chat/chat-page/chat-page.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { LoginPageGuard } from './guards/login-page.guard';
+import { ChatPageGuard } from './guards/chat-page.guard';
 
 const routes: Routes = [
-  { path: 'chat', component: ChatPageComponent },
+  { path: 'chat', component: ChatPageComponent, canActivate: [ChatPageGuard] },
   { path: '**', component: LoginPageComponent, canActivate: [LoginPageGuard] },
 ];
 
