22f5cf69425f183205af0f9a6592422badecbbb2
Rurs21 <rurs21@protonmail.com>
2022-12-18 11:44:53 -0500

    Deploy stuff

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'https://chat-app-qn7duv43oa-nn.a.run.app',
+  wsServer: 'wss://chat-app-qn7duv43oa-nn.a.run.app',
 };
